cc240bf6a3139e877374ca491bb66834dec64431
jimmy9065 <jimmy9065@hotmail.com>
2022-07-18 04:50:01 -0700

    setup cover for pages

--- src/pages/neighborhood.js
+++ src/pages/neighborhood.js
@@ -1,8 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
+import { PageBase, Grid } from "../components/common";
+
+const COVER_URL = 
+"https://casa-barro-public.s3.ap-northeast-2.amazonaws.com/neighborhood/cover.jpg"
+
+const Image = styled.img`
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+`;
+
+const ImageBase = styled.div`
+    width: 100vw;
+    height: 100vh;
+    margin-left: -16px;
+    font-size: 0px;
+`;
+
 let NeighborhoodPage = () => {
-    return (<div>NeighborhoodPage</div>)
+    return (
+        <PageBase>
+            <ImageBase>
+                <Image src={COVER_URL}/>
+            </ImageBase>
+        </PageBase>
+    )
 }
 
 export default NeighborhoodPage
