c7a92d4d24d7958847011cf64e52ad13438a394f
jimmy9065 <jimmy9065@hotmail.com>
2022-07-19 22:46:35 -0700

    finished collaboration page

--- src/pages/collaboration.js
+++ src/pages/collaboration.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { Grid } from "../components/common";
 import Footer from "../components/footer";
 import { COLLABORATION_COVER } from "../constants/remote_files";
+import Logo from "../svgs/CollaborationLogo"
+import BackSlach from "../svgs/backslash";
 
 const COVER_URL = COLLABORATION_COVER
 
@@ -26,12 +28,29 @@ const ImageBase = styled.div`
     font-size: 0px;
 `;
 
+const RowContainer = styled(Grid)`
+    margin-top: 50px;
+    margin-bottom: 100px;
+`
+
+const IconContainer1 = styled.div`
+    grid-column: 2/3;
+`
+
+const IconContainer2 = styled.div`
+    grid-column: 3/7;
+`
+
 let CollaborationPage = () => {
     return (
         <PageContainer>
             <ImageBase>
                 <Image src={COVER_URL}/>
             </ImageBase>
+            <RowContainer>
+                <IconContainer1><BackSlach/></IconContainer1>
+                <IconContainer2><Logo/></IconContainer2>
+            </RowContainer>
             <Footer />
         </PageContainer>
     )
